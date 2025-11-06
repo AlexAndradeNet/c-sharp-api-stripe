c4e2d3d257ca6a8e9627b29dbdcdc9c8ac6a96fd
Alex Andrade <alexander.andrade@gmail.com>
2025-11-06 16:47:16 -0500

    🔧 adding husky pre-hooks

--- install.sh
+++ install.sh
@@ -2,7 +2,14 @@
 
 npm install
 
-cd StripeAPITest
-dotnet restore
 dotnet tool install csharpier
 dotnet tool install dotnet-sonarscanner
+
+dotnet tool install Husky
+dotnet husky install
+
+chmod +x .husky/pre-commit
+chmod +x .husky/pre-push
+
+cd StripeAPITest
+dotnet restore
